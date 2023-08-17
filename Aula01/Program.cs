348214a639236da195383f1e4573f2b7d77d6b41
vinicius.rlamim <vinicius.rlamim@SJC0562921W10-1>
2023-08-17 14:52:34 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Olá mundo!");*///Escreve na tela do console
+            double lado, resultado;
+
+            Console.WriteLine("Digite o valor do lado:");
+            lado = double.Parse(Console.ReadLine());
+            resultado = lado * lado;
+            Console.WriteLine("Resultado é =" +resultado);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
